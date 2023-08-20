a999f384879f7fd9b9f3e2441d1274dcaf7066f8
junggahyun <gahyunjung20@gmail.com>
2023-08-20 19:07:40 +0900

    #17 - 회원 계정 도메인 구현 및 erd 반영
    `UserAccount` 이름으로 회원 계정 도메인 생성
    `user`, `account` 등은 mysql 예약어이므로 피함
    필드명도 mysql 예약어를 의식하여 정함
    
    테스트 데이터에 임의 계정 1개 등록
    테스트용이지만 패스워드가 노출되는 방식이므로 고민 필요
    
    회원 정보는 API 로 노출되지 않았으면 한다.
    이에 테스트에 404 not found 를 확인하는 테스트 추가
    간단하게 모든 http 메소드에 대응

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
+-- 테스트 계정
+-- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
+insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
+    ('gjung', 'asdf1234', 'gjung', 'gjung@mail.com', 'I am gjung.', now(), 'gjung', now(), 'gjung')
+;
+
 -- 123 게시글
 insert into article (title, content, hashtag, created_by, modified_by, created_at, modified_at) values
 ('Praesent blandit lacinia erat.', 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.', null, 'Desmond', 'Maddie', '2023-03-10 18:34:06', '2022-09-11 07:35:16'),
